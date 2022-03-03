612d16dd40c05ba0e26cc444c82923b3c9056c60
ticlekiwi <florian.nicolas@yahoo.fr>
2022-03-03 08:55:24 +0100

    Update v4.js

--- js/bots/v4.js
+++ js/bots/v4.js
@@ -1,13 +1,13 @@
 /**
- * Fucking DUMB bot. 
+ * Ameliorated bot. 
  * Filter by good and bad characters using regex
  * Take advantage of miss placed characters.
+ * Take advantage of known max number specific chars.
  * Select best word using score
  * Score represent:
  * - word sharing the most letters with other possible words
  * - word sharing the most letters in miss placed characters
  * - On first attempt, word having the most vowels. 
- * - On second attempt, try something really stupid.
  */
 class SutomBotV4 extends SutomBotV3 {
     constructor (allWords) {
